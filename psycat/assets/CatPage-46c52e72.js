4090b447a6304f123effd675c30f3b348d41f8cd
Artem Antonov <antonov@saldlab.com>
2023-04-17 21:09:50 +0300

    0.0.8

--- psycat/assets/CatPage-46c52e72.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-5161d2f6.js";const r={};function t(c,_){return" цветовая реакция тест "}const a=e(r,[["render",t]]);export{a as default};
---
+++ psycat/assets/CatPage-a5f304ae.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-9e898206.js";const r={};function t(c,_){return" цветовая реакция тест "}const a=e(r,[["render",t]]);export{a as default};
