7bdb7f78687686838eb83f882a538143cda11705
artem <Artem Antonov>
2024-01-27 21:34:02 +0300

    0.0.38

--- psycat/assets/CameraTest2Page-c520cb79.js
+++ psycat/assets/CameraTest2Page-c520cb79.js
@@ -1,4 +1,4 @@
-import{k as Ii,B as Yc,o as $c,c as Jc,a as Ro}from"./index-b526ef89.js";/**
+import{k as Ii,B as Yc,o as $c,c as Jc,a as Ro}from"./index-a368f95e.js";/**
  * @license
  * Copyright 2020 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
